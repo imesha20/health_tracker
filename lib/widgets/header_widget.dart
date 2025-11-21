e4636ad444b10f89588a820c42b0c50ba02e5af7
imesh <imeshaprabodani2002@gmail.com>
2025-11-21 21:17:22 +0530

    removed search bar and placed name of the app

--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -1,3 +1,4 @@
+
 import 'package:fitness_tracker/constant/colors.dart';
 import 'package:fitness_tracker/utils/responsive.dart';
 import 'package:flutter/material.dart';
@@ -7,88 +8,58 @@ class HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bool isDesktop= Responsive.isDesktop(context);
+    final bool isDesktop = Responsive.isDesktop(context);
 
-    // TODO: implement build
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center, // Center row contents
       children: [
-        if(!isDesktop)
-        GestureDetector(
-          onTap:() {
-           Scaffold.of(context).openDrawer(); 
-
-          },
-          child: const Padding(
-            padding: EdgeInsets.all(8),
-            child:Icon(Icons.menu,color: greyColor,
-            size: 25,
-            ) ,
-            ),
-
-        ),
-
-        Expanded(
-          child:TextField(
-        style:TextStyle(
-          color:greyColor 
-        ) ,
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: cardBgColor,
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.transparent,
+        // Menu icon for non-desktop screens
+        if (!isDesktop)
+          GestureDetector(
+            onTap: () {
+              Scaffold.of(context).openDrawer();
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(8),
+              child: Icon(
+                Icons.menu,
+                color: greyColor,
+                size: 25,
+              ),
             ),
           ),
-        labelStyle: const TextStyle(
-         color: greyColor,
-         ),
 
-         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-         ),
-         focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide(
-            color:Theme.of(context).primaryColor,
-         )
-        ),
-
-        contentPadding: const EdgeInsets.symmetric(
-          vertical: 5,
-        ),
-        hintText: "Search",
-        hintStyle: const TextStyle(
-          color: greyColor,
-        ),
-        prefixIcon: const Icon(Icons.search,color: greyColor,
-        size:21 ,
-        )
-        ),
-
-        ),
-         ),
-         //show the summary page if the screen size is not a descktop size in an end drawer
-         if(!isDesktop)
-         InkWell(
-          onTap: (){
-            Scaffold.of(context).openEndDrawer();
-          },
-           child: Padding(
-            padding:const  EdgeInsets.symmetric(horizontal:20, 
+        // Centered Application Name
+        Expanded(
+          child: Center(
+            child: Text(
+              "Health Mate",
+              style: TextStyle(
+                color: Colors.purple,      // Changed to purple
+                fontSize: 26,              // Slightly larger font
+                fontWeight: FontWeight.bold,
+              ),
             ),
-             child: ClipRRect(
-               borderRadius: BorderRadius.circular(100),
-               child: Image.asset(
-  "assets/icons/images/flowers.png",
-  width: 40,
-),
+          ),
+        ),
 
-             
-             ),
-           ),
-         )
-
+        // Profile / end drawer icon for non-desktop screens
+        if (!isDesktop)
+          InkWell(
+            onTap: () {
+              Scaffold.of(context).openEndDrawer();
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.asset(
+                  "assets/icons/images/flowers.png",
+                  width: 40,
+                ),
+              ),
+            ),
+          ),
       ],
     );
   }
